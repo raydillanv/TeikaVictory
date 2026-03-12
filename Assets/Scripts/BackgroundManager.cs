90b553add7b4cdd88d9d1049b852102c208c1a12
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-03-12 15:11:43 -0500

    Buggy Background

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -6,10 +6,13 @@ public class BackgroundManager : MonoBehaviour
     public GameObject[] bcks;
     public float pivotPoint;
     public float speed;
+    public float scale;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        pivotPoint = scale * 16 * -0.32f;
+        bckPrefab.transform.localScale = new Vector3(scale, scale, 0.0f);
         bcks = new GameObject[3];
         for (int i = 0 ; i < 3; i++)
         {
@@ -23,6 +26,17 @@ public class BackgroundManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0 ; i < 3; i++)
+        {
+            float xPos = bcks[i].transform.position.x + speed;
+            float yPos = bcks[i].transform.position.y + speed;
+            Vector3 pos = new Vector3(xPos, yPos, 0.0f);
+            
+            if (bcks[i].transform.position.x > -pivotPoint / 2)
+            {
+                pos = new Vector3(pivotPoint, pivotPoint, 0.0f);
+            }
+            bcks[i].transform.position = pos;
+        }
     }
 }
